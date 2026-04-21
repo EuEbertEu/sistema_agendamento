832a21ee93d3e5cf1b15ec91c1daac427ba67399
EuEbertEu <ebert334455@gmail.com>
2026-04-21 15:54:06 -0300

    Avanço nas estruturas python e html
    Pude estruturar melhor a estrutura Backend utilizando o Flask e algumas questões visuais

---
+++ iniciar/views.py
@@ -0,0 +1,18 @@
+from iniciar import app
+from flask import render_template
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/agendamento')
+def angendamento():
+    return render_template('agendamento.html')
+
+@app.route('/servicos')
+def servicos():
+    return render_template('serviços.html')
+
+@app.route('/finalizar')
+def finalizar():
+    return render_template('final.html')
